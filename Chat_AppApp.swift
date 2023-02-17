34052eb980c77c10fcc22f2670d52f9e60825ae5
Dinith <codse202f060@student.nibm.lk>
2023-02-17 19:57:25 +0530

    first commit

---
+++ Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_AppApp.swift
+//  Chat App
+//
+//  Created by Dinith Hasaranga on 2023-02-17.
+//
+
+import SwiftUI
+
+@main
+struct Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
